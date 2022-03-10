16607f97a4c9d29e7887c659c9cebf0b8674a912
Joss Dixon <josscdixon@gmail.com>
2022-03-10 08:53:34 +0000

    model associations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has a first name" do
+    user = User.new(
+      first_name: "",
+      last_name: "Smith",
+      email: "hello@mail.com",
+      address: "1 World Street, London"
+    )
+    assert user.valid?
+  end
 end
