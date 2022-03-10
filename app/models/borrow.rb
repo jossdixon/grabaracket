943e4d2b35b2457c01b8e73cbcf096077f63bbda
Joss Dixon <josscdixon@gmail.com>
2022-03-10 09:01:22 +0000

    enums added

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -1,4 +1,6 @@
 class Borrow < ApplicationRecord
   belongs_to :users
   belongs_to :rackets
+
+  enum status: { pending: 0, approved: 1, rejected: 2 }
 end
--- app/models/racket.rb
+++ app/models/racket.rb
@@ -1,3 +1,5 @@
 class Racket < ApplicationRecord
   belongs_to :user
+
+  enum grip_size: { L1: 0, L2: 1, L3: 2, L4: 3, L5: 4 }
 end
