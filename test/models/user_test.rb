b180fa91ca7da3697993d591078b912ba888eecf
Joss Dixon <josscdixon@gmail.com>
2022-03-10 09:12:08 +0000

    user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,58 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "has a first name" do
+  test "invalid without a first name" do
     user = User.new(
       first_name: "",
       last_name: "Smith",
       email: "hello@mail.com",
       address: "1 World Street, London"
     )
-    assert user.valid?
+    user.valid?
+    assert_not user.errors[:first_name].empty?
+  end
+
+  test "invalid with first name shorter than two characters" do
+    user = User.new(
+      first_name: "J",
+      last_name: "Smith",
+      email: "hello@mail.com",
+      address: "1 World Street, London"
+    )
+    user.valid?
+    assert_not user.errors[:first_name].empty?
+  end
+
+  test "invalid without last name" do
+    user = User.new(
+      first_name: "John",
+      last_name: "",
+      email: "hello@mail.com",
+      address: "1 World Street, London"
+    )
+    user.valid?
+    assert_not user.errors[:last_name].empty?
+  end
+
+  test "invalid with last name shorter than two characters" do
+    user = User.new(
+      first_name: "John",
+      last_name: "S",
+      email: "hello@mail.com",
+      address: "1 World Street, London"
+    )
+    user.valid?
+    assert_not user.errors[:last_name].empty?
+  end
+
+  test "invalid with invalid email" do
+    user = User.new(
+      first_name: "John",
+      last_name: "Smith",
+      email: "hello.com",
+      address: "1 World Street, London"
+    )
+    user.valid?
+    assert_not user.errors[:email].empty?
   end
 end
